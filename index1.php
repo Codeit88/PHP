68cfb25ad2735ea3915376336c999c04c459bd04
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2023-06-22 15:40:24 +0500

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "techmaze";
+
+
+$conn = mysqli_connect($server, $username, $password, $database);
+if (!$conn) echo "Error Connecting";
