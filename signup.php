f54608a5fcd3926b2cdf1c89375e1e9b8498f609
Sami Khokher <112092098+Codeit88@users.noreply.github.com>
2023-07-08 16:05:28 +0500

    Add files via upload

--- signup.php
+++ signup.php
@@ -1,20 +1,19 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    include 'index1.php';
-    $not = false;
-    $em = $_POST["email"];
-    $uname = $_POST["fullname"];
-    $pass = $_POST["password"];
-    $cpass = $_POST["confirm_password"];
-    if ($pass == $cpass) {
-        $sql = "INSERT INTO `signup` (`name`, `email`, `pass`, `conf-pass`) VALUES ('$uname', '$em', '$pass', '$cpass')";
-        $res = mysqli_query($conn, $sql);
-    }
-    if ($res) {
-        echo 'Data inserted successfully';
-    } else {
-        echo 'Error: ' . mysqli_error($conn);
-    }
+include 'connection.php';
+$show = false;
+$showerror = false;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $uname = $_POST['fullname'];
+    $num = $_POST['tel'];
+    $pass = $_POST['password'];
+    $confp = $_POST['confirm_password'];
+    if ($pass == $confp) {
+        $con = "INSERT INTO `users` (`name`, `Password`, `Confirm_Pass`, `phone_number`) VALUES ('$uname', '$pass', '$confp', '$num')";
+        $res = mysqli_query($conn, $con);
+        if ($res)
+            $show = true;
+    } else
+        $showerror = true;
 }
 ?>
 <!DOCTYPE html>
@@ -24,18 +23,65 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     <meta charset="UTF-8">
     <title>Create Account</title>
     <link rel="stylesheet" href="signup.css">
+
+
+
+
+
+    <style>
+        .custom-alert {
+            border-radius: 5px;
+            padding: 20px;
+            margin-bottom: 20px;
+            color: #155724;
+            background-color: #d4edda;
+        }
+
+        .custom-alert2 {
+            border-radius: 5px;
+            padding: 20px;
+            margin-bottom: 20px;
+            color: darkred;
+            background-color: salmon;
+        }
+
+
+
+        .custom-alert h4 {
+            margin-top: 0;
+            margin-bottom: 10px;
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+        .custom-alert p {
+            margin-bottom: 0;
+        }
+    </style>
 </head>
 
 <body>
-    <div class="h1">
-        <h1>
-            Welcome to TechMaze! Login before you enter.
-        </h1>
-
-    </div>
-
+    <?php
+    if ($show) {
+        echo '<div class="container mt-4">
+        <div class="custom-alert alert alert-success" role="alert">
+            <h4 class="alert-heading">Well done!</h4>
+            <p>Welldone! Your Account has been created successfully. Go back to Login Page.</p>
+        </div>
+    </div>';
+    }
+    if ($showerror) {
+        echo '<div class="container mt-4">
+        <div class="custom-alert2 alert alert-danger" role="alert">
+            <h4 class="alert-heading">Sorry!</h4>
+            <p>Your entered data is not correct!Try again</p>
+        </div>
+    </div>';
+    }
+    ?>
     <div class="container">
-        <h2>Create Account</h2>
+
+        <h2>Create QuickChat Account</h2>
         <form method="POST">
             <div style="display: none;">
             </div>
@@ -43,8 +89,8 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             <label for="fullname">Full Name</label>
             <input type="text" id="fullname" name="fullname" placeholder="Enter your full name" required>
 
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email address" required>
+            <label for="tel">Number</label>
+            <input type="tel" id="tel" name="tel" placeholder="Enter your Phone Number" required>
 
             <label for="password">Password</label>
             <input type="password" id="password" name="password" placeholder="Enter a strong password" required>
